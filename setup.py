9adfd982b75610c5ed2cbae64c77a299f548d365
Fluxticks <dev.garmentbecky@gmail.com>
2024-03-28 00:40:14 +0000

    Update package setup

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="instagram-dlpy",
-    version="0.0.1",
+    version="1.0.0",
     url="https://github.com/Fluxticks/InstagramDL",
-    download_url="https://github.com/Fluxticks/InstagramDL/archive/v0.0.1.tar.gz",
+    download_url="https://github.com/Fluxticks/InstagramDL/archive/v1.0.0.tar.gz",
     author="Fluxticks",
     packages=find_packages(),
-    install_requires=["playwright",
-                      "bs4",
-                      "lxml"],
-    description="A package to gather post information for a given Instagram post",
+    install_requires=["requests"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="A python package to download Instagram posts by URL without needing to login",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.10"
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    keywords=["instagram",
-              "playwright",
-              "async"],
+    keywords=["instagram", "api"],
 )
